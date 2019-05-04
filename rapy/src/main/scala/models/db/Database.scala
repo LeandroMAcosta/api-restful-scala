f33d8bd5aa57ec17e1348af6f3b0a4522934f8b7
Christian L Moreno <christianluciano.m@gmail.com>
2019-05-04 16:33:03 -0300

    Add Items model and Items database

--- rapy/src/main/scala/models/db/Database.scala
+++ rapy/src/main/scala/models/db/Database.scala
@@ -15,10 +15,12 @@ object Database {
   var baseDir: String = "db"
   private[models] val locations = new DatabaseTable[Location]("locations.json")
   private[models] val users = new DatabaseTable[User]("users.json")
+  private[models] val items = new DatabaseTable[Items]("items.json")
   
   private val databases = List(
     (locations, Location),
     (users, User),
+    (items, Items)
   )
 
   private def loadDatabaseTable(file: String): Try[List[JValue]] = Try {
